b9ffb03a0c092a66de2c0451a662ecf8040f139e
eliza0x <me@eliza.link>
2017-08-11 02:05:50 +0900

    Update: project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject isopen "0.1.0-SNAPSHOT"
+(defproject isopen "0.1.1-SNAPSHOT"
   :main "isopen"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "slack bot"
+  :url "https://github.com/eliza0x/is_open"
+  :license {:name "MIT LICENCE"
+            :url "https://github.com/eliza0x/is_open/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"] 
                  [http-kit "2.0.0"]
                  [aleph "0.4.1"]
